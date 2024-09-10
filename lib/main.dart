925bfb12eeba26401ae0a49ea49f0d4aedd20a2d
Abraao Brandao <abraao.brandao@fpf.br>
2024-09-09 22:51:23 -0400

    estrutura principal

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
@@ -25,23 +24,6 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text('Hello world'),
-        ),
-        body: const Column(children: [
-          Text(
-            'Te amo Abimael',
-            style: TextStyle(color: Colors.red, fontSize: 32, letterSpacing: 4),
-          ),
-          SizedBox(
-            height: 300,
-            width: 300,
-            child: Text(
-              'Segundo texto',
-            ),
-          ),
-        ]));
+    return const Text('Hello world', textAlign: TextAlign.center);
   }
 }
