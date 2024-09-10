ba429f040a004a74c7cc940e2a7e2c49c5ad6f6a
Abraao Brandao <abraao.brandao@fpf.br>
2024-09-10 00:06:51 -0400

    widgets

--- lib/main.dart
+++ lib/main.dart
@@ -24,21 +24,33 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: <Widget>[
-        Text('Hello world'),
-        Text('texto 2'),
-        Row(
-          children: [
-            Icon(
-              Icons.person,
-              color: Colors.white,
-              size: 40,
-            ),
-            Text('texto 2'),
-          ],
-        )
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Barra'),
+      ),
+      body: const Column(
+        children: <Widget>[
+          Text('Hello world'),
+          Row(
+            children: [
+              Icon(
+                Icons.person,
+                size: 30,
+              ),
+              Text('texto 2'),
+            ],
+          ),
+          SizedBox(
+            width: 300,
+            height: 80,
+            child: Icon(Icons.local_pizza),
+          ),
+          Padding(
+            padding: EdgeInsets.all(30),
+            child: Text('mais  um texto'),
+          )
+        ],
+      ),
     );
   }
 }
