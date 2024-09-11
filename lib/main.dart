b615d9f5cd57165f8bc66d9d24a95a936ad2bf38
Abraao Brandao <abraaobritof10@gmail.com>
2024-09-11 14:52:11 -0400

    usando SingleChildScrollView e Flex

--- lib/main.dart
+++ lib/main.dart
@@ -30,42 +30,67 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Barra'),
       ),
-      body: Column(
-        children: <Widget>[
-          const Text('Hello world'),
-          const Row(
-            children: [
-              Icon(
-                Icons.person,
-                size: 30,
-              ),
-              Text('texto 2'),
-            ],
-          ),
-          const SizedBox(
-            width: 300,
-            height: 80,
-            child: Icon(Icons.local_pizza),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(30),
-            child: Text('mais  um texto'),
-          ),
-          Container(
-            alignment: Alignment.topRight,
-            padding: const EdgeInsets.all(20),
-            decoration: const BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.all(Radius.circular(20))),
-            width: 300,
-            child: const Text('container'),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                dev.log('fui clicado');
-              },
-              child: const Text('Eu sou um botão')),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const Text('Hello world'),
+            const Row(
+              children: [
+                Icon(
+                  Icons.person,
+                  size: 30,
+                ),
+                Text('texto 2'),
+              ],
+            ),
+            const SizedBox(
+              width: 300,
+              height: 80,
+              child: Icon(Icons.local_pizza),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(30),
+              child: Text('mais  um texto'),
+            ),
+            Container(
+              alignment: Alignment.topRight,
+              padding: const EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              width: 300,
+              child: const Text('container'),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  dev.log('fui clicado');
+                },
+                child: const Text('Eu sou um botão')),
+            Row(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.green,
+                      height: 50,
+                    )),
+                Expanded(
+                    flex: 3,
+                    child: Container(
+                      color: Colors.grey,
+                      height: 50,
+                    )),
+                Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.yellow,
+                      height: 50,
+                    )),
+              ],
+            ),
+            Image.network('https://picsum.photos/300/900')
+          ],
+        ),
       ),
     );
   }
